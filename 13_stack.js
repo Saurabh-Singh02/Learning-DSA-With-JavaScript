b286c9c915b8ff156fcb11c783a27a32310ae775
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-13 18:46:52 +0000

    commit

--- 13_stack.js
+++ 13_stack.js
@@ -1,6 +1,11 @@
 let data = [];
 let crntSize = data.length;
 let max = 5;
+
+document.getElementById("push_button").onclick = push;
+document.getElementById("pop_button").onclick = function() {myFunction()};
+document.getElementById("display_buttoin").onclick = function() {myFunction()};
+
 function push() {
     let NewValue= document.getElementById("Element").Value;
     if (crntSize >= max) {
