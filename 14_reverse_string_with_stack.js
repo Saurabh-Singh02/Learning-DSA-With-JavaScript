45e7544a8f13ed8bb0ad698f0afc539c293e896d
Saurabh <saur02052000abh@gmail.com>
2023-03-17 22:33:09 +0530

    reverse string with the help of stack

---
+++ 14_reverse_string_with_stack.js
@@ -0,0 +1,27 @@
+let data = [];
+let crntSize = data.length;
+function push(Value) {
+    data[crntSize] = Value;
+    crntSize += 1;
+}
+function pop() {
+    LastRemoveItem = data[crntSize - 1];
+    crntSize -= 1;
+    data.length = crntSize;
+    return LastRemoveItem;
+}
+function reverseStr(item) {
+    for (let index = 0; index < item.length; index++) {
+        push(item[index])
+    }
+    for (let index = 0; index < item.length; index++) {
+        item[index] = pop();
+    }
+}
+
+let str = "Saurabh"
+str = str.split("")
+reverseStr(str);
+console.log(str);
+console.log(str.join(""));
+// console.log(data);
